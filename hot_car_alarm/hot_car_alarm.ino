c5d5a6061ca42e9ec360661bc34cdc8cf4c99fe6
cwo3990 <cwo3990@rit.edu>
2022-10-27 22:36:25 -0400

    made constants for the pins and changed the LEDs to use one pin so they will flash simultaneously

--- hot_car_alarm/hot_car_alarm.ino
+++ hot_car_alarm/hot_car_alarm.ino
@@ -5,11 +5,11 @@
  * -------------------
  */
 #include <LiquidCrystal.h>
-int tempPin = 0;
-int buzzer = 6;
-int ledPin1 = 5;
-int ledPin2 = 4;
-int unsafeTemp = 85;
+#define TEMPERATURE_PIN 0 // ANALOG 0 PIN
+#define BUZZER_PIN 6  // DIGITAL 6
+#define LED_PIN 4         // DIGITAL 4
+
+int unsafeTemp = 29; // Temperature in Celsius
 
 //                BS  E  D4 D5  D6 D7
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);  //LCD display
@@ -19,13 +19,12 @@ void setup() {
   Serial.begin(9600);  // send temperature to computer
   // setup pins for each component
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  int tempReading = analogRead(tempPin);
+  int tempReading = analogRead(TEMPERATURE_PIN);
   double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
   tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK);  //  Temp Kelvin
   float tempC = tempK - 273.15;                                                 //  Convert Kelvin to Celsius
@@ -49,28 +48,28 @@ void loop() {
   if (tempF >= unsafeTemp) {
     // flash LEDs
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(ledPin1, HIGH);   // turn one red LED on
-    digitalWrite(ledPin2, HIGH);   // turn the other red LED on
+    digitalWrite(LED_PIN, HIGH);   // turn one red LED on
+    // digitalWrite(ledPin2, HIGH);   // turn the other red LED on
     delay(200);                       // wait
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(ledPin1, LOW);    // turn one red LED off by making the voltage LOW
-    digitalWrite(ledPin2, LOW);    // turn the other red LED off by making the voltage LOW
+    digitalWrite(LED_PIN, LOW);    // turn one red LED off by making the voltage LOW
+    // digitalWrite(ledPin2, LOW);    // turn the other red LED off by making the voltage LOW
     
     unsigned int i;
     
     // Activate buzzer
     // output a frequency
     for (i = 0; i < 80; i++) {
-      digitalWrite(buzzer, HIGH);  //buzzer on
+      digitalWrite(BUZZER_PIN, HIGH);  //buzzer on
       delay(1);                    // wait for 1ms
-      digitalWrite(buzzer, LOW);   //buzzer off
+      digitalWrite(BUZZER_PIN, LOW);   //buzzer off
       delay(1);                    // wait for 1ms
     }
     // output another frequency
     for (i = 0; i < 100; i++) {
-      digitalWrite(buzzer, HIGH);  // buzzer on
+      digitalWrite(BUZZER_PIN, HIGH);  // buzzer on
       delay(2);                    // wait for 2ms
-      digitalWrite(buzzer, LOW);   // buzzer off
+      digitalWrite(BUZZER_PIN, LOW);   // buzzer off
       delay(2);                    // wait for 2ms
     }
   }
